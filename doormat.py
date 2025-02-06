6055659af896535e3ced64d8337f0d1dcced0b98
sudarshanpadekal <sudarshanpadekal2006@gmail.com>
2025-02-07 02:54:55 +0530

    Update doormat.py

--- doormat.py
+++ doormat.py
@@ -4,12 +4,18 @@ terminal_width = shutil.get_terminal_size().columns
 disp=int(math.floor(((terminal_width-22)/2)))
 
 print(("-"*disp+" Welcome Mat Designer "+"-"*disp).center(terminal_width))
+print(" ")
+print(" ")
 print("#It must be a odd number only.".rjust(terminal_width))
 S=input("Enter the width of the mat you want to design: ")
+print(" ")
+print(" ")
 
 while S.isdigit()==False or 3*int(S)>terminal_width or int(S)%2==0:
     print("TRY AGAIN! DESIGN NOT POSSIBLE!".center(terminal_width))
     S=input("Enter the width of the mat you want to design: ")
+    print(" ")
+    print(" ")
 
 N=int(S)
 M=3*N
@@ -25,3 +31,6 @@ for i in range(1,N+1):
         i=N-i+1
         Z=int(((M-3*(2*i-1))/2))
         print(("-"*Z+".|."*(2*i-1)+"-"*Z).center(terminal_width))
+print(" ")
+print(" ")
+print("-"*terminal_width)
